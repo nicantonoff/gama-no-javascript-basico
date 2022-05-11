3bed15bdc0226c2584868f9b545258ff3c524620
Nicolas Antonoff <nicolas.antonoff@gmail.com>
2022-05-11 20:40:02 -0300

    final da 5a aula

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ function validaCPF(cpf) {
     if(cpf.length != 11) {
         return false;
     } else {
+
+        // Validação do 1o dígito verificador
         var numeros = cpf.substring(0, 9);
         var digitos = cpf.substring(9);
 
@@ -11,11 +13,24 @@ function validaCPF(cpf) {
         for (var i = 10; i > 1; i--) {
             soma += numeros.charAt(10 - i) * i;
         }
-        console.log(soma);
 
         var resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
 
         if(resultado != digitos.charAt(0)) {
+            return false;
+        }
+
+        // Validação do 2o dígito verificador
+        soma = 0;
+        numeros = cpf.substring(0, 10);
+
+        for (var k = 11; k > 1; k--) {
+            soma += numeros.charAt(11 - k) * k;
+        }
+
+        resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
+
+        if(resultado != digitos.charAt(1)) {
             return false;
         }
 
@@ -25,6 +40,8 @@ function validaCPF(cpf) {
 
 function validacao() {
     console.log('iniciando validação');
+    document.getElementById('success').style.display = 'none';
+    document.getElementById('error').style.display = 'none';
 
     var cpf = document.getElementById('cpf_digitado').value;
 
