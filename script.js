7ee4b85141c6dba3b56630ab80a9b737da6f684c
Nicolas Antonoff <nicolas.antonoff@gmail.com>
2022-05-11 16:16:26 -0300

    final da 4a aula

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
 console.log('JS carregado');
 
 function validaCPF(cpf) {
-    return false;
+    if(cpf.length != 11) {
+        return false;
+    } else {
+        var numeros = cpf.substring(0, 9);
+        var digitos = cpf.substring(9);
+
+        var soma = 0;
+        for (var i = 10; i > 1; i--) {
+            soma += numeros.charAt(10 - i) * i;
+        }
+        console.log(soma);
+
+        var resultado = soma % 11 < 2 ? 0 : 11 - (soma % 11);
+
+        if(resultado != digitos.charAt(0)) {
+            return false;
+        }
+
+        return true;
+    }
 }
 
 function validacao() {
     console.log('iniciando validação');
 
     var cpf = document.getElementById('cpf_digitado').value;
-    console.log(cpf);
 
     var resultadoValidacao = validaCPF(cpf);
 
